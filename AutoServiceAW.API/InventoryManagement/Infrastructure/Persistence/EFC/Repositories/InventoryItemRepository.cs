52686444519f2bdbdcb6980c46377bb98a701d3c
Diego <luis.diegoa.03@gmail.com>
2026-06-11 19:26:53 -0500

    feat: add EF Core repository implementation for InventoryItem

---
+++ AutoServiceAW.API/InventoryManagement/Infrastructure/Persistence/EFC/Repositories/InventoryItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using AutoServiceAW.API.InventoryManagement.Domain.Model.Aggregates;
+using AutoServiceAW.API.InventoryManagement.Domain.Repositories;
+using AutoServiceAW.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using AutoServiceAW.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace AutoServiceAW.API.InventoryManagement.Infrastructure.Persistence.EFC.Repositories;
+
+/// <summary>
+/// Infrastructure implementation of the <see cref="IInventoryItemRepository"/> contract 
+/// using Entity Framework Core and inheriting generic operations from <see cref="BaseRepository{InventoryItem}"/>.
+/// </summary>
+public class InventoryItemRepository(AppDbContext context) : BaseRepository<InventoryItem>(context), IInventoryItemRepository
+{
+    
+}
